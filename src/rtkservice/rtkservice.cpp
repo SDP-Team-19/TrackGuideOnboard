e92be67350a36cd9c3d8c0d043d3eb515ce63e5f
Mitchell <belgianlion1234@icloud.com>
2025-03-03 17:59:18 -0500

    Fix command

--- src/rtkservice/rtkservice.cpp
+++ src/rtkservice/rtkservice.cpp
@@ -3,7 +3,7 @@
 RTKService::RTKService(const std::string& config_file) : config_file_(config_file) {}
 
 void RTKService::start_server() const {
-    std::string command = "rtkrcv -o " + config_file_ + " -s -nc --daemon";
+    std::string command = "rtkrcv -o " + config_file_ + " -s -nc --deamon";
     int result = std::system(command.c_str());
     if (result != 0) {
         std::cerr << "Failed to start RTK server with command: " << command << std::endl;
