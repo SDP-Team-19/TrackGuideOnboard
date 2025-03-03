365e0684d9115cabba5e710676457a3321d91f00
Mitchell <belgianlion1234@icloud.com>
2025-03-03 18:05:32 -0500

    Ignore startup message and disable logging on server

--- src/rtkservice/rtkservice.cpp
+++ src/rtkservice/rtkservice.cpp
@@ -3,7 +3,7 @@
 RTKService::RTKService(const std::string& config_file) : config_file_(config_file) {}
 
 void RTKService::start_server() const {
-    std::string command = "rtkrcv -o " + config_file_ + " -s -nc --deamon";
+    std::string command = "rtkrcv -o " + config_file_ + " -t 0 -s -nc --deamon";
     int result = std::system(command.c_str());
     if (result != 0) {
         std::cerr << "Failed to start RTK server with command: " << command << std::endl;
--- src/rtkservice/tcpserver.cpp
+++ src/rtkservice/tcpserver.cpp
@@ -61,20 +61,35 @@ void TCPServer::start() {
 
         std::cout << "Connection received from " << inet_ntoa(client_addr.sin_addr) << std::endl;
 
-        // Fork a new process to handle the client
-        pid_t pid = fork();
-        if (pid == -1) {
-            std::cerr << "Fork failed: " << strerror(errno) << std::endl;
+        // Read the first byte to check if it starts with '%'
+        char initial_byte;
+        ssize_t bytes_received = recv(client_socket, &initial_byte, 1, MSG_PEEK);
+        if (bytes_received == -1) {
+            std::cerr << "recv() error: " << strerror(errno) << std::endl;
             close(client_socket);
             continue;
-        } else if (pid == 0) {
-            // Child process
-            close(server_socket); // Close the listening socket in the child process
-            handle_client(client_socket);
-            exit(EXIT_SUCCESS);
+        }
+
+        if (initial_byte == '%') {
+            // Handle the client in the same process
+            std::cout << "Startup message received from server" << std::endl;
+            close(client_socket);
         } else {
-            // Parent process
-            close(client_socket); // Close the client socket in the parent process
+            // Fork a new process to handle the client
+            pid_t pid = fork();
+            if (pid == -1) {
+                std::cerr << "Fork failed: " << strerror(errno) << std::endl;
+                close(client_socket);
+                continue;
+            } else if (pid == 0) {
+                // Child process
+                close(server_socket); // Close the listening socket in the child process
+                handle_client(client_socket);
+                exit(EXIT_SUCCESS);
+            } else {
+                // Parent process
+                close(client_socket); // Close the client socket in the parent process
+            }
         }
     }
 }
