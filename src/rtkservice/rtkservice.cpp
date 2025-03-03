71ea0b3d8bfdda9108ec0262dd6d3cddfe9347de
Mitchell <belgianlion1234@icloud.com>
2025-03-03 18:33:19 -0500

    Fix definition for rtkrcv

--- src/rtkservice/rtkservice.cpp
+++ src/rtkservice/rtkservice.cpp
@@ -4,7 +4,7 @@
 RTKService::RTKService(const std::string& config_file) : config_file_(config_file) {}
 
 void RTKService::start_server() const {
-    std::string command = "rtkrcv -o " + config_file_ + " -t 0 -s -nc --deamon -p" + std::to_string(PORT) + " -w \"\"";
+    std::string command = "rtkrcv -o " + config_file_ + " -t 0 -s -nc --deamon -w \"\" -p " + std::to_string(PORT);
     int result = std::system(command.c_str());
     if (result != 0) {
         std::cerr << "Failed to start RTK server with command: " << command << std::endl;
