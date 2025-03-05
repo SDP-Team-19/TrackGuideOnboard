03a71f6a9dfdb97462fc6586c55b138eca32fdbc
Mitchell <belgianlion1234@icloud.com>
2025-03-05 11:57:18 -0500

    clear on shutdown

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,14 @@
 #define PORT 12345
 
 RTKService* rtk_service_ptr = nullptr;
+LEDControl* led_control_ptr = nullptr;
 
 #include <iostream>
 
 void signal_handler(int signal) {
     if (signal == SIGINT && rtk_service_ptr) {
         std::cout << "Shutting down RTK service..." << std::endl;
+        led_control_ptr->clear();
         if (rtk_service_ptr->shutdown_server() == 1) {
             std::cerr << "Failed to shut down RTK service.  Please check if the server is running. Try to shut it down gracefully with `telnet localhost 12346` and type `shutdown` (If that does not work, `ps aux | grep [r]tkrcv`, and kill the process)" << std::endl;
         }
@@ -20,6 +22,7 @@ void signal_handler(int signal) {
 
 int main() {
     LEDControl led_control(12, 30);
+    led_control_ptr = &led_control;
     led_control.indicate_left(Color::BLUE);
     led_control.indicate_right(Color::BLUE);
     RTKService rtk_service("/home/team19/RTK_CONFIG/rtkrcv_no_logs.conf");
