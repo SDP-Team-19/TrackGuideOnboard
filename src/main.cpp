5bc9b1a3058452edd1a5e45ff7fc58631344f0d8
Mitchell <belgianlion1234@icloud.com>
2025-03-06 18:39:23 -0500

    bypass if

--- src/main.cpp
+++ src/main.cpp
@@ -38,16 +38,16 @@ int main() {
 
     std::cout << "Shutting down safely..." << std::endl;
 
-    if (shutdown_requested.load()) {
-        if (rtk_service_ptr) {
-            std::cout << "Shutting down rtk service" << std::endl;
-            rtk_service_ptr->shutdown_server();
-        }
-        if (led_control_ptr) {
-            std::cout << "Shutting down led control" << std::endl;
-            led_control_ptr->clear();
-        }
+    //if (shutdown_requested.load()) {
+    if (rtk_service_ptr) {
+        std::cout << "Shutting down rtk service" << std::endl;
+        rtk_service_ptr->shutdown_server();
     }
+    if (led_control_ptr) {
+        std::cout << "Shutting down led control" << std::endl;
+        led_control_ptr->clear();
+    }
+    //}
     // if (button_thread.joinable()) {
     //     button_thread.join();
     // }
