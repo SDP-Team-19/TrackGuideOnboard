7227703055ba7f5743b0d15e784f37f9a6b2f34d
Mitchell <belgianlion1234@icloud.com>
2025-03-11 14:02:05 -0400

    Fix issues

--- src/inputoutput/buttons.cpp
+++ src/inputoutput/buttons.cpp
@@ -77,7 +77,7 @@ void Buttons::monitor_button(std::atomic<bool>& shutdown_requested) {
         prevPlayButtonState_ = playState;
 
         // Add a small delay to prevent high CPU usage
-        gpioDelay(10000); // 10 milliseconds
+        gpioDelay(500000); // 10 milliseconds
     }
     std::cout << "Button monitoring thread shutting down..." << std::endl;
 }
