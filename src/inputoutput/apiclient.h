a3dc11fce8b8487d01e4191babc51599d0fa6864
Mitchell <belgianlion1234@icloud.com>
2025-04-15 17:46:07 -0400

    Fix return type

--- src/inputoutput/apiclient.h
+++ src/inputoutput/apiclient.h
@@ -22,7 +22,7 @@ public:
     nlohmann::json createModeRequest(float threshold, const std::string& mode);
 
     // Send a JSON request to the server and return the response
-    nlohmann::json sendPostRequest(const std::string& url, const nlohmann::json& requestBody);
+    std::string sendPostRequest(const std::string& url, const nlohmann::json& requestBody);
 
 private:
 
