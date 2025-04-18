a764cca9a1cbd7e91410de8796e4072eac4955b8
Mitchell <belgianlion1234@icloud.com>
2025-04-18 15:49:53 -0400

    Disable animation

--- src/rtkservice/tcpserver.cpp
+++ src/rtkservice/tcpserver.cpp
@@ -86,15 +86,15 @@ void TCPServer::start(std::atomic<bool>& shutdown_requested) {
     std::future<void> animation_future;
 
     // Start animation in background
-    auto run_animation = [&shutdown_requested, this]() {
-        while (!shutdown_requested.load(std::memory_order_acquire)) {
-            ledController_.led_location_bounce_animation(Color::BLUE, 3);
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        }
-    };
+    // auto run_animation = [&shutdown_requested, this]() {
+    //     while (!shutdown_requested.load(std::memory_order_acquire)) {
+    //         ledController_.led_location_bounce_animation(Color::BLUE, 3);
+    //         std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    //     }
+    // };
 
-    // Start animation immediately
-    animation_future = std::async(std::launch::async, run_animation);
+    // // Start animation immediately
+    // animation_future = std::async(std::launch::async, run_animation);
 
     // Main loop to accept and handle client connections
     while (!shutdown_requested.load(std::memory_order_acquire)) {
@@ -111,13 +111,13 @@ void TCPServer::start(std::atomic<bool>& shutdown_requested) {
             return;
         }
 
-        if (activity == 0) {
-            // If no connection and animation isn't running, restart it
-            if (!animation_future.valid()) {
-                animation_future = std::async(std::launch::async, run_animation);
-            }
-            continue;
-        }
+        // if (activity == 0) {
+        //     // If no connection and animation isn't running, restart it
+        //     if (!animation_future.valid()) {
+        //         animation_future = std::async(std::launch::async, run_animation);
+        //     }
+        //     continue;
+        // }
 
         client_socket = accept(serverSocket_, (struct sockaddr *)&client_addr, &sin_size);
 
@@ -145,17 +145,17 @@ void TCPServer::start(std::atomic<bool>& shutdown_requested) {
             std::cout << "Startup message received from server" << std::endl;
             close(client_socket);
         } else {
-            if (animation_future.valid()) {
-                animation_future = std::future<void>();
-            }
+            // if (animation_future.valid()) {
+            //     animation_future = std::future<void>();
+            // }
             handle_client(client_socket, shutdown_requested);
         }
     }
 
     // Ensure animation thread is stopped
-    if (animation_future.valid()) {
-        animation_future = std::future<void>();
-    }
+    // if (animation_future.valid()) {
+    //     animation_future = std::future<void>();
+    // }
     ledController_.clear();
     std::cout << "Server shutting down..." << std::endl;
     close_server();
