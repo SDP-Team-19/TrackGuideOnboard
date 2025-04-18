f1e50fbe31919a6a47c502884f56f514038fddb1
Mitchell <belgianlion1234@icloud.com>
2025-04-18 16:18:10 -0400

    Fix code issues

--- src/boundarylogic/boundarylogic.h
+++ src/boundarylogic/boundarylogic.h
@@ -49,7 +49,6 @@ public:
     // Member functions
     double calculate_distance(double latitude, double longitude);
     bool load_track(const std::string& file_path);
-    double get_threshold() const { return _threshold; }
 
 private:
     double computeSignedPerpendicularDistance(const Point2D& userPos, const Point2D& closest, const Point2D& next);
